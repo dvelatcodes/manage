2860ebb0c2f6ff823b4eab2f0b4694283c51f7e6
love olubummo <olubummolove3@gmail.com>
2024-11-10 13:18:00 +0100

    create: light mode and dark mode feature

---
+++ script.js
@@ -0,0 +1,35 @@
+const moon = document.querySelector('.moon');
+const sun = document.querySelector('.sun');
+const hamburger = document.querySelector('.ham');
+const close = document.querySelector('.close');
+const body = document.querySelector('body');
+const genText = document.querySelectorAll('.genText');
+const  a = document.querySelectorAll('a');
+let mode = 'light';
+
+moon.addEventListener('click', ()=>{
+    if (mode === 'light') {
+        moon.style.display = 'none';
+        sun.style.display = 'block';
+        sun.style.color  = 'white';
+        // genText.style.color = 'white';
+        genText.forEach((data)=> {
+            data.style.color = 'white';
+        })
+        body.style.backgroundColor = 'black';
+        mode = 'dark'
+    }
+});
+
+sun.addEventListener('click', ()=>{
+    if (mode === 'dark') {
+        sun.style.display = 'none';
+        moon.style.display = 'block';
+        // genText.style.color = 'black';
+        genText.forEach((data)=> {
+            data.style.color = 'black';
+        })
+        body.style.backgroundColor = 'white';
+        mode = 'light'
+    }
+});
