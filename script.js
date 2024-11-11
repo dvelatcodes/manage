a9f0840d2f0177851599065038af4f1c3077d6f7
love olubummo <olubummolove3@gmail.com>
2024-11-11 11:31:48 +0100

    update: implemented the hamburger function

--- script.js
+++ script.js
@@ -1,47 +1,57 @@
 const moon = document.querySelector('.moon');
 const sun = document.querySelector('.sun');
 const hamburger = document.querySelector('.ham');
-const close = document.querySelector('.close');
+const closeHam = document.querySelector('.close');
 const body = document.querySelector('body');
 const genText = document.querySelectorAll('.genText');
-const  a = document.querySelectorAll('a');
+const a = document.querySelectorAll('a');
 const shadow = document.querySelector('.shadow');
-let hamChecker  = true;
+const nav = document.querySelector('nav');
+let hamChecker = true;
 let mode = 'light';
 
-moon.addEventListener('click', ()=>{
+moon.addEventListener('click', () => {
     if (mode === 'light') {
         moon.style.display = 'none';
         sun.style.display = 'block';
-        sun.style.color  = 'white';
-        // genText.style.color = 'white';
-        genText.forEach((data)=> {
+        sun.style.color = 'white';
+        genText.forEach((data) => {
             data.style.color = 'white';
         })
         body.style.backgroundColor = 'black';
+        hamburger.style.color = 'white';
+        closeHam.style.color = 'white';
+        nav.style.backgroundColor = 'transparent';
         mode = 'dark'
     }
 });
 
-sun.addEventListener('click', ()=>{
+sun.addEventListener('click', () => {
     if (mode === 'dark') {
         sun.style.display = 'none';
         moon.style.display = 'block';
-        // genText.style.color = 'black';
-        genText.forEach((data)=> {
+        genText.forEach((data) => {
             data.style.color = 'black';
         })
         body.style.backgroundColor = 'white';
+        hamburger.style.color = 'black';
+        closeHam.style.color = 'black';
+        nav.style.backgroundColor = 'white';
         mode = 'light'
     }
 });
 
 
-hamburger.addEventListener('click', ()=>{
+hamburger.addEventListener('click', () => {
     hamburger.style.display = 'none';
-
+    closeHam.style.display = 'block';
+    shadow.style.display = 'block';
+    nav.style.display = 'flex';
 })
 
-close.addEventListener('click', ()=>{
-
+closeHam.addEventListener('click', () => {
+    closeHam.style.display = 'none';
+    hamburger.style.display = 'block';
+    shadow.style.display = 'none';
+    nav.style.display = 'none';
 })
