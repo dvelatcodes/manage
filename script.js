12d1c96d622581bcf5b9103a9d719526e98ae097
love olubummo <olubummolove3@gmail.com>
2024-11-22 16:11:30 +0100

    update: js file

--- script.js
+++ script.js
@@ -130,7 +130,7 @@ allAnchor.forEach((a) => {
         //             a.style.backgroundColor = 'rgb(249 115 22)';
         //         }
         //         else{
-        //             a.style.backgroundCo
+        //             a.style.backgroundC
         
         for (let j = 0; j < allAnchor.length; j++) {
             // console.log(allAnchor[0].getAttribute('href'));
