c8f83ffb5e210ccc46748a59c4a3000ae043aa81
love olubummo <olubummolove3@gmail.com>
2024-11-22 16:13:15 +0100

    update: js file

--- script.js
+++ script.js
@@ -121,7 +121,6 @@ let check5 = 0;
 
 allAnchor.forEach((a) => {
     a.addEventListener('click', () => {
-        // console.log(allAnchor.length);
         
         for (let j = 0; j < allAnchor.length; j++) {
             // console.log(allAnchor[0].getAttribute('href'));
